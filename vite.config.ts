3a5af7b636b1edd401ec61043340eb0ba006ad40
hunlongyu <hunlongyu@gmail.com>
2021-06-18 18:23:47 +0800

    v0.1.5 完成打包脚本的修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,34 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
-      name: 'eprComponents'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
+export default ({ mode }) => {
+  if (mode === 'lib') {
+    return {
+      plugins: [vue()],
+      build: {
+        outDir: 'dist',
+        lib: {
+          entry: path.resolve(__dirname, 'src/components/index.ts'),
+          name: 'eprComponents'
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue'
+            }
+          }
         }
       }
     }
   }
-})
+  if (mode === 'doc') {
+    return {
+      plugins: [vue()],
+      build: {
+        outDir: 'docs'
+      }
+    }
+  }
+}
