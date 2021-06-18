59cbc6dcf59e1deb60a99484c9a085e3c569009b
hunlongyu <hunlongyu@gmail.com>
2021-06-18 18:40:11 +0800

    BUG: 开发模式丢失

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,43 @@
-// import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default ({ mode }) => {
-  if (mode === 'lib') {
-    return {
-      plugins: [vue()],
-      build: {
-        outDir: 'dist',
-        lib: {
-          entry: path.resolve(__dirname, 'src/components/index.ts'),
-          name: 'eprComponents'
-        },
-        rollupOptions: {
-          external: ['vue'],
-          output: {
-            globals: {
-              vue: 'Vue'
+export default ({ command ,mode }) => {
+  if (command === 'build') {
+    if (mode === 'lib') {
+      return {
+        plugins: [vue()],
+        build: {
+          outDir: 'dist',
+          lib: {
+            entry: path.resolve(__dirname, 'src/components/index.ts'),
+            name: 'eprComponents'
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue'
+              }
             }
           }
         }
       }
     }
+    if (mode === 'doc') {
+      return {
+        plugins: [vue()],
+        base: './',
+        build: {
+          outDir: 'docs'
+        }
+      }
+    }
   }
-  if (mode === 'doc') {
+  if (command === 'serve') {
     return {
       plugins: [vue()],
-      build: {
-        outDir: 'docs'
+      server: {
+        open: true
       }
     }
   }
